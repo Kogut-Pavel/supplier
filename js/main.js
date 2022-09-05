e359a8689bd9135ced2f5c5b14535cdc328e77a5
Pavel Kogut <73947945+Kogut-Pavel@users.noreply.github.com>
2022-09-05 20:57:17 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// Owl carousel
+
+$('.slide-one').owlCarousel({
+    dots: true,
+    margin: 25, 
+    infinity: false,
+    responsive : {
+        768 : {
+            margin: 15,
+        },
+        320 : {
+
+            items: 1,
+        }
+
+    }
+});
+
+
+let $homeSlider = $(".slide-two");
+ 
+$(window).resize(function() {
+  showHomeSlider();
+});
+ 
+function showHomeSlider() {
+  if ($homeSlider.data("owlCarousel") !== "undefined") {
+    if (window.matchMedia("(max-width: 767px)").matches) {
+      initialHomeSlider();
+    } else {
+      destroyHomeSlider();
+    }
+  }
+}
+showHomeSlider();
+ 
+function initialHomeSlider() {
+  $homeSlider.addClass("owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+  });
+}
+ 
+function destroyHomeSlider() {
+  $homeSlider.trigger("destroy.owl.carousel").removeClass("owl-carousel");
+}
+
+
+
